073358eea0e65f4dd73bd3e8789ecc72bc79d89c
josuawilliams <josuawilliams17@gmail.com>
2022-04-21 12:29:07 +0700

    BUKABESOK

---
+++ models/store.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Store extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.hasMany(models.Product)
+      this.belongsTo(models.User)
+    }
+  }
+  Store.init({
+    name: DataTypes.STRING,
+    category: DataTypes.STRING,
+    code: DataTypes.STRING,
+    since: DataTypes.DATE,
+    UsersId : DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Store',
+  });
+  return Store;
+};
